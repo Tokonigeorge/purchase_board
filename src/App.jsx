dce88d1eafe70d279af67d198710fd2f2c0a4328
Bukola <bukolaebikefe@yahoo.com>
2022-02-09 14:15:39 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App h-screen">
-      <p>Testing</p>
+      <p className="font-bold text-4xl text-purp">Complete your purchase</p>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+const Nav = () => {
+  return (
+    <div className="flex items-center">
+      <p>Personal Info</p>
+      <p>Billing Info</p>
+      <p>Confirm Payment</p>
+    </div>
+  );
+};
+
+export default Nav;
