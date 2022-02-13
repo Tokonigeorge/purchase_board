773b9c7301dfa530f6ca9fff7693bac6ddaa1e5e
Bukola <bukolaebikefe@yahoo.com>
2022-02-13 21:53:52 +0100

    card input field formatting

--- src/components/billingInfo/BillingInfo.jsx
+++ src/components/billingInfo/BillingInfo.jsx
@@ -4,12 +4,45 @@ import TextInput, { Label, textInputStyle } from "../TextInput";
 
 const BillingInfo = ({ handleNav, handleCancel }) => {
   const [cardDetails, setCardDetails] = useState("");
+  const [expire, setExpire] = useState("");
+  const [cvv, setCvv] = useState("");
   const [options, setOptions] = useState("Visa");
 
   const cards = ["Visa", "MasterCard", "Verve"];
 
   const validateDetails = (e) => {
-    setCardDetails(e.target.value);
+    setCardDetails(formatCardNumber(e.target.value));
+  };
+
+  const handleExpireChange = (e) => {
+    // ignore invalid input
+    if (!/^\d{0,2}\/?\d{0,2}$/.test(e.target.value)) {
+      return;
+    }
+
+    let input = e.target.value;
+
+    if (/^\d{3,}$/.test(input)) {
+      input = input.match(new RegExp(".{1,2}", "g")).join("/");
+    }
+
+    setExpire(input);
+  };
+
+  const handleCvChange = (e) => {
+    //accept only numbers in field
+    if (e.target.value == "" || /^[0-9]*$/.test(e.target.value))
+      setCvv(e.target.value);
+  };
+
+  //format value, adding space after each 5 character
+  const formatCardNumber = (value) => {
+    const regex = /^(\d{0,5})(\d{0,5})(\d{0,5})(\d{0,5})$/g;
+    const onlyNumbers = value.replace(/[^\d]/g, "");
+
+    return onlyNumbers.replace(regex, (regex, $1, $2, $3, $4) =>
+      [$1, $2, $3, $4].filter((group) => !!group).join(" ")
+    );
   };
 
   return (
@@ -56,18 +89,24 @@ const BillingInfo = ({ handleNav, handleCancel }) => {
               <input
                 id="Expiry Date"
                 type={"text"}
+                inputMode="numeric"
                 maxLength={5}
                 placeholder="04/23"
+                value={expire}
                 className={textInputStyle}
+                onChange={(e) => handleExpireChange(e)}
               />
             </div>
             <div className="w-12">
               <Label label={"CVV"} isNecessary={true} />
               <input
                 id="CVV"
-                type={"text"}
+                type={"tel"}
+                inputMode="numeric"
                 maxLength={3}
                 placeholder="923"
+                value={cvv}
+                onChange={(e) => handleCvChange(e)}
                 className={`${textInputStyle} text-black `}
               />
             </div>
